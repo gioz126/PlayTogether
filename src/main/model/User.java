c5d293a7940c5488b4dcaed8586b17b7fd197003
Giovittorio Bernie <giovittoriogio@gmail.com>
2025-10-02 21:00:45 -0700

    Add User, Booking, Session, Community and Court in model package.

---
+++ src/main/model/User.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS:
+    public User(String name, String contactNumber, String sportInterest) {
+        // stub
+    }
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS:
+    public void bookCourt(Court c, LocalDateTime starTime, LocalDateTime endTime) {
+        // stub
+    }
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS:
+    public void createSession(String sport, LocalDateTime starTime, LocalDateTime endTime) {
+        // stub
+    }
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS:
+    public void joinSession(Session session) {
+        // stub
+    }
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS:
+    public void createCommunity(String sport, String area) {
+        // stub
+    }
+
+    // REQUIRES:
+    // MODIFIES:
+    // EFFECTS:
+    public void joinCommunity(String prefSport, String prefArea) {
+        // stub
+    }
+
+}
