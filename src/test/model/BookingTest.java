111a52ed8d207f62c06e97aa52fe5b0942949d18
Giovittorio Bernie <giovittoriogio@gmail.com>
2025-10-04 10:02:23 -0700

    Add constructor and getter test for Booking class

---
+++ src/test/model/BookingTest.java
@@ -0,0 +1,42 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BookingTest {
+
+    private Booking bookingTest;
+    private User testUser;
+    private CourtFacility facility;
+    private CourtUnit court1;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    @BeforeEach
+    public void runBefore() {
+        testUser = new User("John", "1293", SportType.BADMINTON);
+        facility = new CourtFacility("GYM", "VANCOUVER");
+        court1 = new CourtUnit("BADMINTON1", SportType.BADMINTON,
+                LocalTime.of(8, 0), LocalTime.of(22, 0));
+
+        start = LocalDateTime.of(2025, 10, 15, 12, 0);
+        end = LocalDateTime.of(2025, 10, 15, 14, 0);
+
+    }
+
+    @Test
+    public void constructorTest() {
+        bookingTest = new Booking(testUser, facility, court1, start, end);
+        assertEquals(testUser, bookingTest.getUser());
+        assertEquals(facility, bookingTest.getFacility());
+        assertEquals(court1, bookingTest.getCourt());
+        assertEquals(start, bookingTest.getStartTime());
+        assertEquals(end, bookingTest.getEndTime());
+    }
+
+}
