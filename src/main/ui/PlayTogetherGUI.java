30505873a61f0e4a76d4a53578a7d81c0d97d683
Giovittorio Bernie <giovittoriogio@gmail.com>
2025-11-11 22:05:30 -0800

    add stub method to ask user to load app state and method to ask user to login

--- src/main/ui/PlayTogetherGUI.java
+++ src/main/ui/PlayTogetherGUI.java
@@ -4,11 +4,16 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import java.awt.BorderLayout;
+import java.time.LocalTime;
 
+import model.AreaLocation;
 import model.CommunityManager;
+import model.CourtFacility;
 import model.CourtFacilityManager;
+import model.CourtUnit;
 import model.PlayTogetherState;
 import model.SessionManager;
+import model.SportType;
 import model.User;
 import model.UserManager;
 import persistence.JsonReader;
@@ -38,14 +43,49 @@ public class PlayTogetherGUI extends JFrame {
         this.setLayout(new BorderLayout());
         this.setVisible(true);
 
-        //setup managers
+        // setup managers
         userManager = new UserManager();
         sessionManager = new SessionManager();
         communityManager = new CommunityManager();
         facilityManager = new CourtFacilityManager();
 
+        setupCourts();
+
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE, facilityManager);
+
+        // ask to load
+        askToLoadState();
+
+        //ask user to login or register
+        askUserToLogin();
+
+        
+    }
+
+    private void askUserToLogin() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'askUserToLogin'");
+    }
+
+    private void askToLoadState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'askToLoadState'");
+    }
+
+    private void setupCourts() {
+        CourtFacility badmintonVancouver = new CourtFacility("UBC North Recreation Badminton", AreaLocation.VANCOUVER);
+        badmintonVancouver.addCourt(new CourtUnit("Badminton 1", SportType.BADMINTON,
+                LocalTime.of(8, 0), LocalTime.of(22, 0)));
+        badmintonVancouver.addCourt(new CourtUnit("Badminton 2", SportType.BADMINTON,
+                LocalTime.of(8, 0), LocalTime.of(22, 0)));
+
+        CourtFacility padelRichmond = new CourtFacility("Padel Richmond", AreaLocation.RICHMOND);
+        padelRichmond.addCourt(new CourtUnit("Padel 1", SportType.PADEL, LocalTime.of(8, 0), LocalTime.of(22, 0)));
+        padelRichmond.addCourt(new CourtUnit("Padel 2", SportType.PADEL, LocalTime.of(8, 0), LocalTime.of(22, 0)));
+
+        facilityManager.addFacility(badmintonVancouver);
+        facilityManager.addFacility(padelRichmond);
     }
 
     public static void main(String[] args) {
